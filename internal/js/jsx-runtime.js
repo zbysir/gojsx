cdb6b8c487691dfefa7d10049362ccc8ac48b252
zhangliang <zbysir@qq.com>
2022-08-22 17:08:59 +0800

    fix className

--- internal/js/jsx-runtime.js
+++ internal/js/jsx-runtime.js
@@ -5,35 +5,11 @@ export function jsx(nodeName, attributes) {
       attributes,
     }
   } else {
-    let n = nodeName(attributes)
-    let na = n.attributes
-    if (na) {
-      if (attributes.className) {
-        if (na['className']) {
-          na['className'] = [na['className'], attributes.className]
-        } else {
-          na['className'] = attributes.className
-        }
-      }
-      if (attributes.style) {
-        na['style'] = Object.assign(na['style'], attributes.style)
-      }
-    } else {
-      na = {}
-      if (attributes.className) {
-        na['className'] = attributes.className
-      }
-      if (attributes.style) {
-        na['style'] = attributes.style
-      }
-    }
-    n.attributes = na
-    return n
+    return nodeName(attributes)
   }
 }
 
 export function jsxs(nodeName, attributes) {
-  // console.log('jsxs', nodeName, JSON.stringify(attributes))
   let x = jsx(nodeName, attributes)
   x.jsxs = true
   return x
